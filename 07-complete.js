dc5946da405afd79023b0fd972558b5b9abdf2d6
nate.online <nate96taylor@gmail.com>
2026-03-18 17:48:52 -0600

    Level 7 complete

---
+++ 07-complete.js
@@ -0,0 +1,16 @@
+import fs from 'fs/promises';
+
+fetch('https://challenge.aptive.tech/api/challenge/cac886c2-e3b7-4028-b8eb-605aa8f3a499/7', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({
+		"can_satisfy_quorum": true,
+		"consensus_value": "consistent",
+		"needs_repair": false,
+		"repair_nodes": []
+	}),
+})
+	.then(response => response.json())
+	.then(data => fs.writeFile('response.json', JSON.stringify(data, null, 4)));
