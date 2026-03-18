aee7ab9ec052bafbc94d8427c366600ccd9e51a1
nate.online <nate96taylor@gmail.com>
2026-03-18 16:09:27 -0600

    Level 3 complete

---
+++ 03-complete.js
@@ -0,0 +1,21 @@
+import fs from 'fs/promises';
+
+const sequence = [0, 1];
+
+// Start by looking for the 2nd index (3rd number)
+for (let i = 2; i < 19; i++) {
+	sequence.push(sequence[i-2] + sequence[i-1]);
+}
+console.log(sequence.at(-1));
+
+fetch('https://challenge.aptive.tech/api/challenge/cac886c2-e3b7-4028-b8eb-605aa8f3a499/3', {
+	method: 'POST',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+	body: JSON.stringify({
+		value: sequence.at(-1)
+	})
+})
+	.then(response => response.json())
+	.then(data => fs.writeFile('response.json', JSON.stringify(data, null, 4)));
